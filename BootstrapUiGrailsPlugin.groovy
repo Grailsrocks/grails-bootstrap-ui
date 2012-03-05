51206e3ff9c3e6792452d49244c2ab73d24fa33b
Marc Palmer <marc@anyware.co.uk>
2012-03-05 21:22:27 +0000

    Updated to work with platform-ui plugin

--- BootstrapUiGrailsPlugin.groovy
+++ BootstrapUiGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class BootstrapUiGrailsPlugin {
     // the plugin version
     def version = "1.0-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.7 > *"
+    def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
@@ -38,7 +38,7 @@ Twitter Bootstrap based UI Set for plugin platform
     }
 
     def doWithConfig = { config ->
-        pluginPlatform {
+        platformUi {
             ui.Bootstrap.button.cssClass = 'btn'
             ui.Bootstrap.tab.cssClass = 'tab-pane'
             ui.Bootstrap.field.cssClass = 'input'
