873c8950ed8858ba180019a2b2009523019a4479
Marc Palmer <marc@anyware.co.uk>
2012-11-26 13:48:35 +0000

    Version 1.0.RC1

--- BootstrapUiGrailsPlugin.groovy
+++ BootstrapUiGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class BootstrapUiGrailsPlugin {
     // the plugin version
-    def version = "1.0-SNAPSHOT"
+    def version = "1.0.RC1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
